278f6e4908ed7d83158327e7afce3de701bad496
Andreas Fink <andreas@fink.org>
2017-04-24 08:12:23 +0200

    Initial Commit

---
+++ mtp3transitfilter/mtp3transitfilter.h
@@ -0,0 +1,19 @@
+//
+//  mtp3transitfilter.h
+//  mtp3transitfilter
+//
+//  Created by Andreas Fink on 24.04.17.
+//  Copyright © 2017 Andreas Fink. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for mtp3transitfilter.
+FOUNDATION_EXPORT double mtp3transitfilterVersionNumber;
+
+//! Project version string for mtp3transitfilter.
+FOUNDATION_EXPORT const unsigned char mtp3transitfilterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mtp3transitfilter/PublicHeader.h>
+
+
